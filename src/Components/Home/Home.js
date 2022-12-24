2996f6e66e1b63fb24fa894ce90a9683e340b7bf
vahide <vahidefarzane@gmail.com>
2022-12-24 13:53:29 +0330

    services added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    // <Swiper
+    //   // install Swiper modules
+    //   modules={[Navigation, Pagination, A11y]}
+    //   spaceBetween={70}
+    //   slidesPerView={1}
+    //   navigation
+    //   pagination={{ clickable: true }}
+    //   onSwiper={(swiper) => console.log(swiper)}
+    //   onSlideChange={() => console.log("slide change")}
+    // >
+    //   <SwiperSlide><img src="./logo.png" alt="" /></SwiperSlide>
+    //   <SwiperSlide><img src="./images/enamad.png" alt="" /></SwiperSlide>
+    //   <SwiperSlide><img src="./images/kasbokar.png" alt="" /></SwiperSlide>
+    //   <SwiperSlide><img src="./images/samandehi.png" alt="" /></SwiperSlide>
+      
+    // </Swiper>
+    <div>
+      
+    </div>
+  );
 }
---
+++ src/Components/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Services.css'
+
+export default function Services() {
+    const services = [
+        {}
+    ]
+  return (
+    <div>Services</div>
+  )
+}

2a57a9b5932a4ab7fc70c1ffdabaf0f18216add0
vahide <vahidefarzane@gmail.com>
2023-01-04 18:16:10 +0330

    productList updated

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import {AiFillStar} from 'react-icons/ai'
+import {RiShareForwardBoxFill} from 'react-icons/ri'
 
-export default function Product() {
+export default function Product(props) {
+  const { productImage, productTtile, productPrice } = props;
   return (
-    <div>
-        
+    <div className="product-container">
+      <img src={productImage} alt="" />
+      <div className="product-details">
+        <h3>{productTtile}</h3>
+        <div class="product-stock_box">
+          <div class="stock-archive">
+            <RiShareForwardBoxFill className="stock-icon"/>
+            <span class="in-stock">موجود در انبار</span>
+          </div>
+          <div class="product-rating-box">
+            <AiFillStar className="raiting-icon"/>
+            <AiFillStar className="raiting-icon"/>
+          </div>
+        </div>
+        <div className="product-price-box">
+          <del className="product-price-last">{productPrice}</del>
+          <span className="product-price-new">{productPrice} تومان</span>
+          <span className="product-price-off">12%</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/Components/ProductsList/ProductsList.js
+++ src/Components/ProductsList/ProductsList.js
@@ -1,64 +1,80 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
+import Product from "../Product/Product";
+import { BiSortDown } from "react-icons/bi";
 import "./ProductsList.css";
 
 export default function ProductsList() {
-  // const { allProducts, ispending } = useFetch(
-  //   "https://fakestoreapi.com/products"
-  // );
   const [categories, setAllcategories] = useState("");
   const [categoriesIsPending, setCategoriesIsPending] = useState(false);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("https://fakestoreapi.com/products/categories")
-  //     .then((category) => {
-  //       console.log(category.data);
-  //       setAllcategories(category.data);
-  //       setCategoriesIsPending(true);
-  //     });
-  // }, []);
+  const { allProducts, ispending } = useFetch(
+    "https://fakestoreapi.com/products"
+  );
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/categories")
+      .then((category) => {
+        console.log(category.data);
+        setAllcategories(category.data);
+        setCategoriesIsPending(true);
+      });
+  }, []);
   return (
     <div className="product-list-container">
       <div className="product-list-sidebar">
         <div className="product-list-filter-price">
-          <h1>فیلتر بر اساس قیمت</h1>
+          <h2>فیلتر بر اساس قیمت</h2>
           <input type="range" />
           <div className="show-filter-price-range">
-            <span>500,000</span>
-            <span>100,000</span>
+            <span className="maximum-price">500,000</span>
+            <span className="minimum-price">100,000</span>
           </div>
           <button>صافی</button>
         </div>
         <div className="product-list-search">
-          <h1>جستجو در محصولات</h1>
+          <h2>جستجو در محصولات</h2>
           <div className="product-list-search-box">
             <input type="search" />
             <button>جستجو</button>
           </div>
         </div>
         <div className="product-categories-list">
-          <h1>فیلتر بر اساس دسته بندی</h1>
+          <h2>فیلتر بر اساس دسته بندی</h2>
           {categoriesIsPending &&
             categories.map((category) => (
               <>
-                <input type="checkbox" name="favorite_pet" value={category} />
-                <span>{category}</span>
+                <input type="checkbox" name="category" value={category} />
+                <span className="product-category-sidebar">{category}</span>
                 <br />
               </>
             ))}
         </div>
       </div>
       <div className="product-list-box">
-        <div className="filtered-tab"></div>
-        <h1>مرتب سازی بر اساس </h1>
-        <ul>
-          <li>محبوب ترین</li>
-          <li>پر بازدید ترین</li>
-          <li>ارزان ترین</li>
-          <li>گران ترین</li>
-        </ul>
+        <div className="product-list-head">
+          <BiSortDown className="sort-icon-haed" />
+          <h2>مرتب سازی بر اساس: </h2>
+          <ul className="product-filtered-head-title">
+            <li>پیشفرض</li>
+            <li>محبوب ترین</li>
+            <li>پر بازدید ترین</li>
+            <li>ارزان ترین</li>
+            <li>گران ترین</li>
+          </ul>
+        </div>
+        <div className="all-products-list">
+          {ispending &&
+            allProducts.map((product) => (
+              <Product
+                className="product-wrapper"
+                key={product.id}
+                productImage={product.image}
+                productTtile={product.title}
+                productPrice={product.price}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );

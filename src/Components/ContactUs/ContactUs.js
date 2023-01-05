94460b97446186080283050c6aecd7f31ed03357
vahide <vahidefarzane@gmail.com>
2023-01-05 14:32:08 +0330

    contact-us page created

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import './ContactUs.css'
 
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
-  )
+    <div class="contact-us-page">
+      <p>
+        سلام خدمت شما کاربر گرامی،
+        <br />
+        برای ارتباط با مدیریت می‌توانید از پل های ارتباطی زیر استفاده نموده و یا
+        از طریق فرم مقابل اقدام فرمایید:
+      </p>
+      <p>آدرس ایمیل: vahidefarzane@gmail.com</p>
+      <p>شماره تلفن تماس: 0915000000</p>
+      <h3>
+        <strong>ساعات کاری:</strong>
+      </h3>
+      <p>شنبه تا چهارشنبه 8 صبح تا 18 عصر؛ پنج شنبه‌ها ساعت 8 صبح تا 12 ظهر</p>
+    </div>
+  );
 }

cd5f437ed0158e4b90466bf31241904254089139
vahide <vahidefarzane@gmail.com>
2023-01-19 19:30:43 +0330

    contactUs component  refactored

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,22 +1,30 @@
+import { Stack, Box, Typography } from "@mui/material";
 import React from "react";
 import "./ContactUs.css";
-import MyButton from "../MyButton/MyButton";
 
 export default function ContactUs() {
   return (
-    <div class="contact-us-page">
-      <p>
+    <Stack sx={{ padding: "2rem 4rem" }}>
+      <Typography
+        component={"p"}
+        sx={{ marginBottom: "1rem", lineHeight: "2rem" }}
+      >
         سلام خدمت شما کاربر گرامی،
         <br />
         برای ارتباط با مدیریت می‌توانید از پل های ارتباطی زیر استفاده نموده و یا
         از طریق فرم مقابل اقدام فرمایید:
-      </p>
-      <p>آدرس ایمیل: vahidefarzane@gmail.com</p>
-      <p>شماره تلفن تماس: 0915000000</p>
-      <h3>
-        <strong>ساعات کاری:</strong>
-      </h3>
-      <p>شنبه تا چهارشنبه 8 صبح تا 18 عصر؛ پنج شنبه‌ها ساعت 8 صبح تا 12 ظهر</p>
-    </div>
+      </Typography>
+      <Typography>آدرس ایمیل: vahidefarzane@gmail.com</Typography>
+      <Typography>شماره تلفن تماس: 0915000000</Typography>
+      <Typography
+        component={"h3"}
+        sx={{ marginTop: "1rem", lineHeight: "2rem" }}
+      >
+        <Typography component={"strong"}>ساعات کاری:</Typography>
+      </Typography>
+      <Typography component={"p"} sx={{ fontSize: "0.9rem" }}>
+        شنبه تا چهارشنبه 8 صبح تا 18 عصر؛ پنج شنبه‌ها ساعت 8 صبح تا 12 ظهر
+      </Typography>
+    </Stack>
   );
 }

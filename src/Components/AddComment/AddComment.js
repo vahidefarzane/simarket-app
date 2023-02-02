8331e70549e2fa4e2ea6c25fda2be796c84e763e
vahide <vahidefarzane@gmail.com>
2023-02-02 16:20:06 +0330

    componentSlider added

--- src/Components/AddComment/AddComment.js
+++ src/Components/AddComment/AddComment.js
@@ -1,7 +1,85 @@
-import React from 'react'
+import { Stack, Box, Typography, Divider } from "@mui/material";
+import { React, useState } from "react";
+import CommentSlider from "../CommentSlider/CommentSlider";
 
 export default function AddComment() {
+  const [sliderInfo, setSliderInfo] = useState([
+    { id: 1, title: "کیفیت ساخت" },
+    { id: 2, title: "ارزش خرید به نسبت قیمت" },
+    { id: 3, title: "امکانات و قابلیت ها" },
+    { id: 4, title: "سهولت استفاده" },
+  ]);
+
   return (
-    <div>AddComment</div>
-  )
+    <Stack sx={{ padding: "2rem 1rem" }}>
+      <Box sx={{ display: "flex" ,flexDirection:{
+        md:'row',
+        xs:'column'
+      }}}>
+        <Box
+          sx={{
+            width: {
+              lg:'30%',
+              md:'35%',
+              xs:'100%',
+            },
+            display: "flex",
+            justifyContent: "center",
+            justifyItems: "center",
+            padding:{
+              lg:'1rem',
+              md:'2rem',
+              xs:'1rem'
+            }
+          }}
+        >
+          <Box
+            component={"img"}
+            src="./images/jacket.jpg"
+            sx={{ width: {
+              lg:'75%',
+              xs:'65%'
+
+            } ,height:{
+              md:'270px',
+              xs:'290px'
+            }}}
+          ></Box>
+        </Box>
+        <Box
+          sx={{
+            width: {
+              md:'70%',
+              xs:'100%',
+            },
+            margin: {
+              md: "0",
+              xs: "0 1rem",
+            },
+          }}
+        >
+          <Typography
+            component={"h1"}
+            sx={{
+              fontSize: "1.3rem",
+              fontWeight: "600",
+              marginBottom: "1.5rem",
+            }}
+          >
+            تی شرت مردانه منان شهیدی مدل
+          </Typography>
+          <Divider />
+          <Box sx={{ display: "flex", flexWrap: "wrap", marginTop: "1.5rem" }}>
+            {sliderInfo.map((slider) => (
+              <CommentSlider
+                key={slider.id}
+                title={slider.title}
+                value={slider.value}
+              />
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </Stack>
+  );
 }
---
+++ src/Components/CommentSlider/CommentSlider.js
@@ -0,0 +1,97 @@
+import { React, useState, useEffect } from "react";
+import { Box, Typography, Slider } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const SliderStyled = styled(Slider)(({ theme }) => ({
+  color: "#ff6a00",
+
+  [theme.breakpoints.up("md")]: {
+    width: "17rem",
+  },
+  [theme.breakpoints.between("sm","md")]: {
+    width: "16rem",
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: "15rem",
+  },
+}));
+
+export default function CommentSlider(props) {
+  const [satisfactionValue, setSatisfactionValue] = useState(50);
+  const [satisfactionName, setSatisfactionName] = useState("نسبتا خوب");
+
+  const SliderName = () => {
+    switch (satisfactionValue) {
+      case 0:
+        setSatisfactionName("خیلی بد");
+        break;
+      case 25:
+        setSatisfactionName("بد");
+        break;
+      case 50:
+        setSatisfactionName("نسبتا خوب");
+        break;
+      case 75:
+        setSatisfactionName("خوب");
+        break;
+      case 100:
+        setSatisfactionName("عالی");
+        break;
+    }
+  };
+  const sliderChanged = (event) => {
+    setSatisfactionValue(event.target.value);
+  };
+  useEffect(() => {
+    SliderName();
+  }, [satisfactionValue]);
+
+  return (
+    <Box
+      sx={{
+        width: {
+          lg: "23rem",
+          md: "21rem",
+          xs: "23rem",
+        },
+        margin: {
+          lg: "0 0 0.8rem 2rem",
+          md: "0 0 0.8rem 1.5rem",
+          xs: "0 0 0.8rem 1rem",
+        },
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: {
+            md: "1rem",
+            xs: "0.9rem",
+          },
+          color: "#737373",
+        }}
+      >
+        {props.title}
+      </Typography>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <SliderStyled
+          aria-label="Temperature"
+          step={25}
+          value={satisfactionValue}
+          onChange={(event) => sliderChanged(event)}
+        />
+        <Typography
+          sx={{
+            marginRight: "1rem",
+            fontSize: {
+              md: "0.9rem",
+              xs: "0.8rem",
+            },
+            color: "#737373",
+          }}
+        >
+          {satisfactionName}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
